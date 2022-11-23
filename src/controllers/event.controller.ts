258e19d00c7ecdcb3618c3457af20c5a5c407813
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-11-23 10:50:08 +0100

    OK getEvents

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,12 +1,15 @@
-import { info } from 'console';
 import { Request, Response, NextFunction } from 'express';
 import { getUserName } from '../functions/getUserName';
 import { isZoneAuthorized } from '../functions/isZoneAuthorized';
 import Logging from '../library/logging';
 import EmployeeModel from '../model/Employee.model';
 import Event from '../model/event.model';
+import Enclos from '../model/enclosure.model';
+import Specie from '../model/specie.model';
+import Animal from '../model/animal.model';
+import Zone from '../model/zone.model';
 
-const NAMESPACE = 'Event';
+const NAMESPACE = 'EVENT';
 
 const createEvent = async (req: Request, res: Response, next: NextFunction) => {
     const { enclosure, specie, animal, eventType, observations } = req.body;
@@ -131,4 +134,213 @@ const createEvent = async (req: Request, res: Response, next: NextFunction) => {
     }
 };
 
-export default { createEvent };
+const getEventsByZone = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const zoneId = req.params.id;
+
+    try {
+        const zone = await Zone.findById(zoneId).exec();
+
+        if (!zone) {
+            Logging.info(NAMESPACE, 'Zone inexistante : ' + zoneId);
+            return res.status(404).json({
+                message: 'Zone inexistante : ' + zoneId
+            });
+        }
+
+        const enclos = await Enclos.find({ zone: zoneId })
+            .select('zone')
+            .exec();
+
+        Logging.info(NAMESPACE, 'Enclos de la zone ' + zoneId + ' : ' + enclos);
+
+        // S'il n'y a aucun enclos dans la zone demandée, envoi d'une réponse NOK
+        if (enclos.length === 0) {
+            return res.status(404).json({
+                message: 'Aucun enclos dans la zone :  ' + zone.name
+            });
+        }
+
+        // Récupération des évènements liés à la zone demandée
+        const events = await Event.find({ enclosure: { $in: enclos } }).exec();
+
+        // S'il n'y a aucun évènement, envoi d'une reponse OK indiquant qu'aucun évènement n'existe pour cette zone
+        if (events.length === 0) {
+            Logging.info(
+                NAMESPACE,
+                'Aucun évènements sur la zone : ' + zone.name
+            );
+            return res.status(200).json({
+                message: 'Aucun évènement sur la zone : ' + zone.name
+            });
+        }
+
+        Logging.info(
+            NAMESPACE,
+            'Evenements de la zone ' + zone.name + ' : ' + events
+        );
+
+        return res.status(200).json({
+            message: 'Evenements de la zone ' + zone.name + ' : ',
+            events
+        });
+    } catch (error) {
+        return res.status(404).json({
+            message:
+                'Impossible de récupérer les évènements de la zone :  ' +
+                zoneId,
+            error
+        });
+    }
+};
+
+const getEventsByEnclosure = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const enclosureId = req.params.id;
+
+    try {
+        const enclosure = await Enclos.findById(enclosureId).exec();
+
+        if (!enclosure) {
+            Logging.info(NAMESPACE, "Enclos inexistant' : " + enclosureId);
+            return res.status(404).json({
+                message: 'Enclos inexistant :  ' + enclosureId
+            });
+        }
+
+        const events = await Event.find({ enclosure: enclosureId }).exec();
+
+        if (events.length === 0) {
+            Logging.info(
+                NAMESPACE,
+                "Aucun évènement sur l'enclos' : " + enclosure.name
+            );
+            return res.status(200).json({
+                message: "Aucun évènement sur l'enclos : " + enclosure.name
+            });
+        }
+
+        Logging.info(
+            NAMESPACE,
+            "Evenements de l'enclos " + enclosure.name + ' : ' + events
+        );
+        return res.status(200).json({
+            message: "Evènements pour l'enclos " + enclosure.name + ' : ',
+            events
+        });
+    } catch (error) {
+        return res.status(404).json({
+            message:
+                'Impossible de récupérer les évènements de la zone :  ' +
+                enclosureId,
+            error
+        });
+    }
+};
+
+const getEventsBySpecie = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const specieId = req.params.id;
+    try {
+        const specie = await Specie.findById(specieId).exec();
+
+        if (!specie) {
+            Logging.info(NAMESPACE, 'Espèce inexistante : ' + specieId);
+            return res.status(404).json({
+                message: 'Espèce inexistante : ' + specieId
+            });
+        }
+
+        const events = await Event.find({ specie: specieId }).exec();
+
+        if (events.length === 0) {
+            Logging.info(
+                NAMESPACE,
+                "Aucun évènement pour l'espèce : " + specie.name
+            );
+            return res.status(200).json({
+                message: "Aucun évènement pour l'espèce : " + specie.name
+            });
+        }
+
+        Logging.info(
+            NAMESPACE,
+            "Evenements pour l'espèce " + specie.name + ' : ' + events
+        );
+        return res.status(200).json({
+            message: "Evènements pour l'espèce " + specie.name + ' : ',
+            events
+        });
+    } catch (error) {
+        return res.status(404).json({
+            message:
+                "Impossible de récupérer les évènements de l'espèce :  " +
+                specieId,
+            error
+        });
+    }
+};
+
+const getEventsByAnimal = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const animalId = req.params.id;
+
+    try {
+        const animal = await Animal.findById(animalId).exec();
+
+        if (!animal) {
+            Logging.info(NAMESPACE, 'Individu inexistant : ' + animalId);
+            return res.status(404).json({
+                message: 'Individu inexistant : ' + animalId
+            });
+        }
+
+        const events = await Event.find({ animal: animalId }).exec();
+
+        if (events.length === 0) {
+            Logging.info(
+                NAMESPACE,
+                "Aucun évènement pour l'individu : " + animal.name
+            );
+            return res.status(200).json({
+                message: "Aucun évènement pour l'individu : " + animal.name
+            });
+        }
+
+        Logging.info(
+            NAMESPACE,
+            "Evenements pour l'individu " + animal.name + ' : ' + events
+        );
+        return res.status(200).json({
+            message: "Evènements pour l'individu " + animal.name + ' : ',
+            events
+        });
+    } catch (error) {
+        return res.status(404).json({
+            message:
+                "Impossible de récupérer les évènements de l'individu :  " +
+                animalId,
+            error
+        });
+    }
+};
+
+export default {
+    createEvent,
+    getEventsByZone,
+    getEventsByEnclosure,
+    getEventsBySpecie,
+    getEventsByAnimal
+};
--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -2,13 +2,39 @@ import express from 'express';
 import controller from '../controllers/event.controller';
 import validationMiddleware from '../middleware/validation.middleware';
 import validate from '../validation/event.validation';
+import extractJWT from '../middleware/extractJWT';
 
 const router = express.Router();
 
 router.post(
     '/creer',
+    extractJWT('Soigneur', 'Responsable', 'Vétérinaire', 'Admin'),
     validationMiddleware(validate.register),
     controller.createEvent
+);
+
+router.get(
+    '/zones/:id',
+    extractJWT('Soigneur', 'Responsable', 'Vétérinaire', 'Admin'),
+    controller.getEventsByZone
+);
+
+router.get(
+    '/enclos/:id',
+    extractJWT('Soigneur', 'Responsable', 'Vétérinaire', 'Admin'),
+    controller.getEventsByEnclosure
+);
+
+router.get(
+    '/especes/:id',
+    extractJWT('Soigneur', 'Responsable', 'Vétérinaire', 'Admin'),
+    controller.getEventsBySpecie
+);
+
+router.get(
+    '/animaux/:id',
+    extractJWT('Soigneur', 'Responsable', 'Vétérinaire', 'Admin'),
+    controller.getEventsByAnimal
 );
 
 export = router;
